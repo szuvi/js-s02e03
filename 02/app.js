e0bd13941b18a4c9fe9a25cf103457d328899cbb
unknown <trzcinskiphoto@gmail.com>
2021-04-18 18:55:10 +0200

    Initial commit for 02

---
+++ 02/app.js
@@ -0,0 +1,12 @@
+// Chess board. Program is to choose at random one of the chess piece
+// (except the pawn) and place it at the random spot on the board.
+// After placing any piece (except first one) check all present
+// pieces move ranges and see if any can reach other piece.
+// If so Give that pieces position and quit program.
+// FACTORY (with class hierarhy) + SINGLETON pattern
+
+// ChessBoard class
+// checcPiece parent class (add white and black!)
+// how long should the program run? ask Karol
+// chessPieces extending paren class (movements defined)
+//
